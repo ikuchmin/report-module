111b72442f01515caea50e8fe426985d973f1780
Ilya Kuchmin <ikuchmin@osslabs.ru>
2015-11-20 09:48:33 +0400

    Model for spu isn't complete

---
+++ src/main/java/ru/osslabs/modules/report/domain/spu/SubService.java
@@ -0,0 +1,81 @@
+package ru.osslabs.modules.report.domain.spu;
+
+import java.util.List;
+
+/**
+ * Created by ikuchmin on 19.11.15.
+ */
+public class SubService {
+
+    private String name;
+    private PeriodSubService period;
+    private List<String> reasonRejectDocuments;
+    private List<String> reasonReject;
+    private List<String> reasonSuspend;
+    private Unit<Integer, String> suspendCount;
+    private PaymentSubService payment;
+    private List<String> methodsGet;
+    private List<String> methodsGetResult;
+
+    public SubService(String name, PeriodSubService period,
+                      List<String> reasonRejectDocuments,
+                      List<String> reasonReject,
+                      List<String> reasonSuspend,
+                      Unit<Integer, String> suspendCount,
+                      Unit<Integer, String> unitSuspend,
+                      PaymentSubService payment,
+                      List<String> methodsGet,
+                      List<String> methodsGetResult) {
+        this.name = name;
+        this.period = period;
+        this.reasonRejectDocuments = reasonRejectDocuments;
+        this.reasonReject = reasonReject;
+        this.reasonSuspend = reasonSuspend;
+        this.suspendCount = suspendCount;
+        this.payment = payment;
+        this.methodsGet = methodsGet;
+        this.methodsGetResult = methodsGetResult;
+    }
+
+    public static class PaymentSubService {
+        private String name;
+        private List<String> npa;
+        private String KBKMFC;
+        private String KBKOGV;
+
+        public PaymentSubService(String name, List<String> npa, String KBKMFC, String KBKOGV) {
+            this.name = name;
+            this.npa = npa;
+            this.KBKMFC = KBKMFC;
+            this.KBKOGV = KBKOGV;
+        }
+    }
+
+    public static class PeriodSubService {
+        private Unit<Integer, String> period;
+        private Unit<Integer, String> periodExTerr;
+
+        public PeriodSubService(Unit<Integer, String> period, Unit<Integer, String> periodExTerr) {
+            this.period = period;
+            this.periodExTerr = periodExTerr;
+        }
+    }
+
+    public static class Unit<T, U> {
+        private T value;
+        private U unit;
+
+        public Unit(T value, U unit) {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public U getUnit() {
+            return unit;
+        }
+    }
+}
--- src/main/java/ru/osslabs/modules/report/fetchers/SPUSubServicesDataFetcher.java
+++ src/main/java/ru/osslabs/modules/report/fetchers/SPUSubServicesDataFetcher.java
@@ -1,48 +1,88 @@
 package ru.osslabs.modules.report.fetchers;
 
-import javaslang.Tuple;
-import javaslang.Tuple2;
 import ru.osslabs.model.datasource.DataObject;
+import ru.osslabs.model.datasource.DataObjectField;
 import ru.osslabs.model.datasource.ExternalDataSource;
 import ru.osslabs.modules.report.decorators.BetweenDateReport;
+import ru.osslabs.modules.report.domain.spu.SubService;
 import ru.osslabs.modules.report.functions.Fetcher;
 
 import javax.inject.Inject;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toMap;
 
 /**
  * Created by ikuchmin on 18.11.15.
  */
-public class SPUSubServicesDataFetcher implements Fetcher<BetweenDateReport, Stream<Tuple2<String, Integer>>> {
+public class SPUSubServicesDataFetcher implements Fetcher<BetweenDateReport, Stream<SubService>> {
 
     @Inject
     ExternalDataSource dataSource;
 
     @Override
     @SuppressWarnings("unchecked")
-    public Stream<Tuple2<String, Integer>> compose(BetweenDateReport report) {
+    public Stream<SubService> compose(BetweenDateReport report) {
         Integer serviceId = 4023;
 
         List<DataObject> subServices = (List<DataObject>) dataSource.getObject("services", serviceId).getFields().get("Servicecommunication").getValue();
 
-        // TODO: Replace stream() on parallel execution after CMDBuildWrapperWS is synchronized. Ticket: #9676
-        Map<String, Integer> countInstanceObjects = cmdClasses.stream()
-                .collect(toMap(
-                        (cmdClass) -> cmdClass,
-                        (cmdClass) -> dataSource.getObjectList(cmdClass, null).size())); // Collect is used because in return iterate on cmdClasses
+        return subServices.stream().map(this::createObjectSubService);
 
-        // TODO: Replace stream() on parallel execution after CMDBuildWrapperWS is synchronized. Ticket: #9676
-        Map<String, String> descriptionClasses = cmdClasses.stream()
-                .collect(toMap(
-                        (cmdClass) -> cmdClass,
-                        (cmdClass) -> dataSource.getObjectType(cmdClass).getName()));
+//        // TODO: Replace stream() on parallel execution after CMDBuildWrapperWS is synchronized. Ticket: #9676
+//        Map<String, Integer> countInstanceObjects = cmdClasses.stream()
+//                .collect(toMap(
+//                        (cmdClass) -> cmdClass,
+//                        (cmdClass) -> dataSource.getObjectList(cmdClass, null).size())); // Collect is used because in return iterate on cmdClasses
+//
+//        // TODO: Replace stream() on parallel execution after CMDBuildWrapperWS is synchronized. Ticket: #9676
+//        Map<String, String> descriptionClasses = cmdClasses.stream()
+//                .collect(toMap(
+//                        (cmdClass) -> cmdClass,
+//                        (cmdClass) -> dataSource.getObjectType(cmdClass).getName()));
+//
+//        return cmdClasses.stream().map((cmdClass) ->
+//                Tuple.of(descriptionClasses.get(cmdClass), countInstanceObjects.get(cmdClass)));
+    }
 
-        return cmdClasses.stream().map((cmdClass) ->
-                Tuple.of(descriptionClasses.get(cmdClass), countInstanceObjects.get(cmdClass)));
+    private SubService createObjectSubService(DataObject dataObject) {
+        Map<String, DataObjectField> fields = dataObject.getFields();
+        return new SubService(
+                getFieldAndCast(fields, "namesubservice"),
+                new SubService.PeriodSubService(
+                        new SubService.Unit<>(
+                                getFieldAndCast(fields, "periodsubservice"),
+                                getFieldAndCast(fields, "FormPeriodSubservice")),
+                        new SubService.Unit<>(
+                                getFieldAndCast(fields, "PeriodSubservice_ExTerr"),
+                                getFieldAndCast(fields, "FormPeriodSubservice_ExTer"))),
+                createCollectionOnAttribute(getFieldAndCast(fields, "reject_noRecept"), "Description"),
+                createCollectionOnAttribute(getFieldAndCast(fields, "rejection_noProv"), "Description"),
+                createCollectionOnAttribute(getFieldAndCast(fields, "rejection_noAct"), "Description"),
+                new SubService.Unit<>(
+                        getFieldAndCast(fields, "suspension_days"),
+                        getFieldAndCast(fields, "FormSuspension_days")),
+                null,
+                null,
+                null,
+                null);
+    }
+
+    private List<String> createCollectionOnAttribute(List<DataObject> collection, String field) {
+        return collection.stream()
+                .map(DataObject::getFields)
+                .map((obj) -> this.<String>getFieldAndCast(obj, field))
+                .collect(Collectors.toList());
+    }
+
+
+    private <R> R getFieldAndCast(Map<String, DataObjectField> fields, String value) {
+        return cast(fields.get(value).getValue());
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <R> R cast(Object obj) {
+        return (R) obj;
     }
 }
